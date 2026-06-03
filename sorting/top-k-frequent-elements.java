5a031eb6d6f0bc1636e2ba5fb8c43337a79d212f
PALLAVI PANDEY <210857824+PALLAVIPANDEY664@users.noreply.github.com>
2026-06-03 16:10:05 +0530

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

--- sorting/top-k-frequent-elements.java
+++ sorting/top-k-frequent-elements.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Interger> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0)+1);
         }
