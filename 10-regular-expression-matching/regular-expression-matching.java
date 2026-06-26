24b09659d530628c84df4589bbeac315f849999b
PALLAVI PANDEY <210857824+PALLAVIPANDEY664@users.noreply.github.com>
2026-06-26 22:45:23 +0530

    Time: 3 ms (25.33%) | Memory: 43.8 MB (23.49%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        
+        // dp[i][j] will be true if s[0..i-1] matches p[0..j-1]
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+        // Base case: empty string matches empty pattern
+        dp[0][0] = true;
+        
+        // Deals with patterns like a*, a*b*, a*b*c* matching an empty string
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // If characters match, or pattern has '.', look diagonally up-left
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } 
+                // If we encounter a '*' wildcard
+                else if (p.charAt(j - 1) == '*') {
+                    // Case 1: Count '*' and its preceding element as 0 occurrences
+                    dp[i][j] = dp[i][j - 2];
+                    
+                    // Case 2: If the preceding element matches the current character in s
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
