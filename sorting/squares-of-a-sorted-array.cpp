2dfc712df951adba745b270acfea56876b5ecc9d
PALLAVI PANDEY <210857824+PALLAVIPANDEY664@users.noreply.github.com>
2026-08-06 23:23:31 +0530

    LC-squares-of-a-sorted-array [Runtime: N/A; Memory: N/A]

--- sorting/squares-of-a-sorted-array.cpp
+++ sorting/squares-of-a-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
         vector<int> result[n];
-        int i = 0, int j = n-1;
+        int i = 0, j = n-1;
         int k = n-1;
         while(k>=0){
             int a = nums[i]* nums[i];
