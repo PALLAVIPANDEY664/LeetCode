f59f756a8559ad7ba658b92a0afd3ee3b576436d
PALLAVI PANDEY <210857824+PALLAVIPANDEY664@users.noreply.github.com>
2026-08-13 16:56:59 +0530

    LC-linked-list-random-node [Runtime: N/A; Memory: N/A]

---
+++ linked-list/linked-list-random-node.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode head;
+    private Random random;
+
+    public Solution(ListNode head) {
+        this.head = head;
+        this.random = new Random();
+    }
+
+    public int getRandom() {
+        ListNode curr = head;
+        int answer = curr.val;
+        int i = 1;
+
+        while (curr != null) {
+            if (random.nextInt(i) == 0) {
+                answer = curr.val;
+            }
+
+            curr = curr.next;
+            i++;
+        }
+
+        return answer;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
